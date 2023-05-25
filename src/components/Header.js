4ac2f1feac66ca8b0c6ed1524b6ededa92899fb9
MKantida <97874677+MKantida@users.noreply.github.com>
2023-05-25 16:26:25 +0700

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,49 @@
 import React ,{useState} from 'react'
 import Logo from '../img/Logo.png'
 import './Header.css'
+import { FiMenu, FiX } from "react-icons/fi";
 
 function Header() {
 
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    console.log(click);
+    const closeMobileMenu = () => setClick(false);
 
     return (
         <div className="header">
-            <div className="container-h">
-                <div className="header-con">
+            <div className="header-h">
+                    
+                    <div className="header-con">
                     <div className="logo-container">
-                            <img src={Logo} alt="Logo"  />   
+                            <img src={Logo} alt="Logo"  />    
                     </div>
-                        <ul className="menu">
-                            <li className="menu-link">
+                        <ul className={click ? "menu active" : "menu"}>
+                            <li className="menu-link" onClick={closeMobileMenu}>
                                 <a href='#'><b>About</b></a>
                             </li>
-                            <li className="menu-link">
+                            <li className="menu-link" onClick={closeMobileMenu}>
                                 <a href='#'><b>Pricing</b></a>
                             </li>
-                            <li className="menu-link">
+                            <li className="menu-link" onClick={closeMobileMenu}>
                                 <a href='#'><b>Contact Us</b></a>
                             </li>
-                            <li className="menu-link">
+                            <li className="menu-link" onClick={closeMobileMenu}>
                                 <a href='#'><b>Login</b></a>
                             </li>
-                            <li className="menu-link">
+                            <li className="menu-link" onClick={closeMobileMenu}>
                                  <a href='#'><button className="button"><b>Stsrt free trial</b></button></a>
 
                                 
                             </li>
                         </ul>
+                        <div className="mobile-menu" onClick={handleClick}>
+                        {click ? (
+                            <FiX />
+                        ) : (
+                            <FiMenu />
+                        )}
+                    </div>
                 </div>
         </div>
     </div>
